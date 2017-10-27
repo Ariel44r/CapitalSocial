3758451f92d106954535c2c1d0342b3fa40498df
Ariel Ramírez <aramirez@iMac-de-Armit-2.local>
2017-10-26 19:25:43 -0500

    Initial Commit

---
+++ CapitalSocial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CapitalSocial
+//
+//  Created by Ariel Ramírez on 26/10/17.
+//  Copyright © 2017 Ariel Ramírez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
