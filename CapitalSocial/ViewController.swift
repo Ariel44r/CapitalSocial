ac9ada671582df0ee841a49fb7ac009920071e0c
Ariel Ramírez <aramirez@iMac-de-Armit-2.local>
2017-10-31 19:48:01 -0600

    firstCommitAfterFSDKSwift

--- CapitalSocial/ViewController.swift
+++ CapitalSocial/ViewController.swift
@@ -7,22 +7,26 @@
 //
 
 import UIKit
+import FacebookLogin
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
-    //MARK: actionButtons
-    @IBAction func buttonLogInFacebook(_ sender: Any) {
-    }
-    @IBAction func buttonLogInPhone(_ sender: Any) {
-        logInPhoneRequest()
-    }
+    //MARK: variablesAndInstances
+    var dict : [String : AnyObject]!
     
     //MARK: outlets
     @IBOutlet weak var textFieldPhone: UITextField!
     
+    //MARK: actionButtons
+    @IBAction func buttonLogInPhone(_ sender: Any) {
+        logInPhoneRequest()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        faceBookLogInButton()
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,6 +49,60 @@ class ViewController: UIViewController {
             }
         }
     }
-    
 }
+
+extension ViewController {
+    func faceBookLogInButton() {
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        //adding it to view
+        view.addSubview(loginButton)
+    }
+    
+    func customLogInButton() {
+        // Add a custom login button to your app
+        let myLoginButton = UIButton(type: .custom)
+        myLoginButton.backgroundColor = UIColor.darkGray
+        myLoginButton.frame = CGRect(x: 0, y: 0, width: 180, height: 40)
+        myLoginButton.center = view.center
+        myLoginButton.setTitle("LoginButton", for: .normal)
+        // Handle clicks on the button
+        myLoginButton.addTarget(self, action: #selector(self.loginButtonClicked), for: .touchUpInside)
+        // Add the button to the view
+        view.addSubview(myLoginButton)
+    }
+    @objc func loginButtonClicked() {
+        let loginManager = LoginManager()
+        loginManager.logIn(readPermissions: [ .publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                self.getFBUserData()
+                debugPrint("LOGIN SUCCESS")
+            }
+        }
+    }
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil) {
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.dict = result as! [String : AnyObject]
+                    debugPrint("DICTIONARY: \(self.dict!)")
+                }
+            })
+        }
+    }
+    func accessToken() {
+        if let accessToken = FBSDKAccessToken.current(){
+            getFBUserData()
+            debugPrint(accessToken.appID)
+            self.performSegue(withIdentifier: "promosSegue", sender: nil)
+        }
+    }
+}
+
+
 
