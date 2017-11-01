fa766a0cc621c9a12faee3f7a6e12b9b917295f4
Ariel Ramírez <aramirez@iMac-de-Armit-2.local>
2017-10-31 20:55:15 -0600

    fix_facebookLogin

--- CapitalSocial/ViewController.swift
+++ CapitalSocial/ViewController.swift
@@ -23,6 +23,28 @@ class ViewController: UIViewController {
         logInPhoneRequest()
     }
     
+    @IBAction func facebookButtonClicked(_ sender: Any) {
+        let loginManager = LoginManager()
+        loginManager.logIn(readPermissions: [.publicProfile], viewController: self, completion: {
+            loginResult in
+            switch loginResult {
+            case .failed(let error):
+                debugPrint(error)
+                break
+            case .cancelled:
+                StaticMethod.PKHUD.errorAndTextHUD(Constants.messagesToUser.cancelledLogin)
+                break
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                StaticMethod.PKHUD.successHUD()
+                self.getFBUserData()
+                if self.accessTokenValidation() {
+                    self.performSegue(withIdentifier: "promosSegue", sender: nil)
+                }
+                break
+            }
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -53,54 +75,44 @@ class ViewController: UIViewController {
 
 extension ViewController {
     func faceBookLogInButton() {
-        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        let loginButton = UIButton(frame: CGRect(x: 40, y: 200, width: 100, height: 40))
+        //let loginButton = LoginButton(readPermissions: [ .publicProfile ])
         loginButton.center = view.center
         //adding it to view
-        view.addSubview(loginButton)
+        //view.addSubview(loginButton)
     }
     
-    func customLogInButton() {
+    /*func customLogInButton() {
         // Add a custom login button to your app
         let myLoginButton = UIButton(type: .custom)
-        myLoginButton.backgroundColor = UIColor.darkGray
+        myLoginButton.backgroundColor = UIColor.blue
         myLoginButton.frame = CGRect(x: 0, y: 0, width: 180, height: 40)
         myLoginButton.center = view.center
         myLoginButton.setTitle("LoginButton", for: .normal)
         // Handle clicks on the button
-        myLoginButton.addTarget(self, action: #selector(self.loginButtonClicked), for: .touchUpInside)
+        myLoginButton.addTarget(self, action: #selector(loginButtonClicked), for: .touchUpInside)
         // Add the button to the view
+        getFBUserData()
         view.addSubview(myLoginButton)
-    }
-    @objc func loginButtonClicked() {
-        let loginManager = LoginManager()
-        loginManager.logIn(readPermissions: [ .publicProfile ], viewController: self) { loginResult in
-            switch loginResult {
-            case .failed(let error):
-                print(error)
-            case .cancelled:
-                print("User cancelled login.")
-            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
-                self.getFBUserData()
-                debugPrint("LOGIN SUCCESS")
-            }
-        }
-    }
+    }*/
+    
+    
     func getFBUserData(){
         if((FBSDKAccessToken.current()) != nil) {
             FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
-                if (error == nil){
+                if (result != nil){
                     self.dict = result as! [String : AnyObject]
                     debugPrint("DICTIONARY: \(self.dict!)")
                 }
             })
         }
     }
-    func accessToken() {
+    func accessTokenValidation() -> Bool {
         if let accessToken = FBSDKAccessToken.current(){
-            getFBUserData()
             debugPrint(accessToken.appID)
-            self.performSegue(withIdentifier: "promosSegue", sender: nil)
+            return true
         }
+        return false
     }
 }
 
