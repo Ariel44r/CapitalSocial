45a6bdf21931cb3b03adada5faebc4bd260c3da4
Ariel Ramírez <aramirez@iMac-de-Armit-2.local>
2017-11-06 17:06:46 -0600

    test_annotation_2

--- CapitalSocial/MapKitFiles/MapKitViewController.swift
+++ CapitalSocial/MapKitFiles/MapKitViewController.swift
@@ -28,22 +28,10 @@ class MapKitViewController: UIViewController {
         let coordinate1 = CLLocationCoordinate2D(latitude: 19.29888, longitude: -99.36861)
         let annotation1 = Annotation("title1","location1", coordinate1)
         let coordinate2 = CLLocationCoordinate2D(latitude: 19.0, longitude: -99.39)
-        let annotation2 = Annotation("title1","location1", coordinate2)
+        let annotation2 = Annotation("title2","location2", coordinate2)
         annotations.append(annotation1)
         annotations.append(annotation2)
         MapView.addAnnotations(annotations)
-    }
-    
-    func mapPlace1() {
-        let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
-        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(19.29888, -99.36861)
-        let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
-        MapView.setRegion(region, animated: true)
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-        annotation.title = "visit me!"
-        annotation.subtitle = "hey!"
-        MapView.addAnnotation(annotation)
     }
     
     override func didReceiveMemoryWarning() {
@@ -64,17 +52,3 @@ class MapKitViewController: UIViewController {
 
 }
 
-class Annotation: NSObject, MKAnnotation {
-    let title: String?
-    let locationName: String
-    //let discipline: String
-    let coordinate: CLLocationCoordinate2D
-    
-    init(_ title: String, _ locationName: String, _ coordinate: CLLocationCoordinate2D) {
-        self.title = title
-        self.locationName = locationName
-        //self.discipline = discipline
-        self.coordinate = coordinate
-        super.init()
-    }
-}
