b3e99ec1ecc794127bd9bb75045f95438a109f24
Ariel Ramírez <aramirez@iMac-de-Armit-2.local>
2017-11-10 18:21:47 -0600

    add_Twitter_Share

--- CapitalSocial/DetailViewFiles/ShareViewController.swift
+++ CapitalSocial/DetailViewFiles/ShareViewController.swift
@@ -11,6 +11,42 @@ import Social
 
 class ShareViewController: UIViewController {
 
+    //MARK: variablesAndInstances
+    let namePhoto: String? = "PromoIdea@1.5x"
+    
+    //MARK: actions
+    @IBAction func shareFacebook(_ sender: Any) {
+        if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeFacebook) {
+            debugPrint("Facebook is Available")
+            let post = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+            post?.setInitialText("Post of the day")
+            if let namePhoto = self.namePhoto {
+                post?.add(UIImage(named: namePhoto))
+            }
+            self.present(post!, animated: true, completion: nil)
+        }
+        else {
+            StaticMethod.PKHUD.errorAndTextHUD("You are not logged in to your Facebook account.")
+        }
+    }
+    @IBAction func shareTwitter(_ sender: Any) {
+        if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter) {
+            debugPrint("Twitter is Available")
+            let post = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            post?.setInitialText("Post of the day")
+            if let namePhoto = self.namePhoto {
+                post?.add(UIImage(named: namePhoto))
+            }
+            self.present(post!, animated: true, completion: nil)
+        }
+        else {
+            StaticMethod.PKHUD.errorAndTextHUD("You are not logged in to your Twitter account.")
+        }
+    }
+    @IBAction func shareWhatsApp(_ sender: Any) {
+        
+    }
+    
     @IBAction func backButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
